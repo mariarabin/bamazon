2596fbc04a8cbe0e3a158b8e0a6d7c80a36d8cd0
Maria <lrabin@gmail.com>
2019-06-07 23:54:28 -0400

    added displayProducts

--- bamazon.js
+++ bamazon.js
@@ -19,7 +19,7 @@ connection.connect(function (err, res) {
     if (err) throw err;
     console.log("---------------Connected as ID: " + connection.threadId + "---------------\n");
     displayProducts();
-    connection.end();
+    //connection.end();
 });
 
 
@@ -27,6 +27,42 @@ function displayProducts() {
     console.log("----------All Available Items for SALE----------\n");
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        res.forEach(row => console.log(`${row.product_name}: ${row.department_name}: ${row.price}: ${row.stock_quantity}`));
+        res.forEach(row => console.log(`${row.item_id}: ${row.product_name}: ${row.department_name}: ${row.price}: ${row.stock_quantity}`));
+        start();
     });
 };
+
+
+function start() {
+    inquirer
+        .prompt({
+            name: "buyOrnot",
+            type: "list",
+            message: "Would you like to buy an item?",
+            choices: ["Buy", "Exit"]
+        })
+        .then(function (answer) {
+            if (answer.buyOrnot === "Buy") {
+                buyItem();
+            }
+            else if (answer.buyOrnot === "Exit") {
+                connection.end();
+            }
+        });
+}
+
+
+function buyItem() {
+    inquirer
+        .prompt({
+            name: "qty",
+            type: "input",
+            message: "How many items do you want to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        });
+}
